58db8baea8ba0e7f63e29d80afd0cdd00c45a10a
Kazuaki Tsukamoto <tsukamoto@seltech.co.jp>
2018-08-27 14:37:20 +0900

    chat: using template

--- chat/main.go
+++ chat/main.go
@@ -3,24 +3,30 @@ package main
 import (
   "log"
   "net/http"
+  "sync"
+  "path/filepath"
+  "text/template"
 )
 
-func main() {
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte (`
-      <html>
-        <head>
-          <title>Chat</title>
-        </head>
-        <body>
-          Let's Chat!
-        </body>
-      </html>
-      `))
-    })
+type templateHandler struct {
+  once sync.Once
+  filename string
+  temp1 *template.Template
+}
 
-    // Start web server
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-      log.Fatal("ListenAndServe:", err)
-    }
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  t.once.Do(func () {
+    t.temp1 =
+      template.Must(template.ParseFiles(filepath.Join("template", t.filename)))
+  })
+  t.temp1.Execute(w, nil)
+}
+
+func main() {
+  // root
+  http.Handle("/", &templateHandler{filename: "chat.html"})
+  // Start web server
+  if err := http.ListenAndServe(":8080", nil); err != nil {
+    log.Fatal("ListenAndServe:", err)
+  }
 }
