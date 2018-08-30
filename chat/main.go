9ab6c4053efc7c55a35a604de6a6ff0e7160e218
Kazuaki Tsukamoto <tsukamoto@seltech.co.jp>
2018-08-30 18:57:05 +0900

    chat: using WebSocket

--- chat/main.go
+++ chat/main.go
@@ -23,8 +23,12 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-  // root
+  r := newRoom()
+
   http.Handle("/", &templateHandler{filename: "chat.html"})
+  http.Handle("/room", r)
+
+  go r.run()
   // Start web server
   if err := http.ListenAndServe(":8080", nil); err != nil {
     log.Fatal("ListenAndServe:", err)
